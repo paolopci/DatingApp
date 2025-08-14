8c2b7199ae6ea2946d561e17446c8ec73202fb88
Paolo Paci <paolopci@yahoo.it>
2025-08-14 15:20:52 +0200

    add bootstrap 5.3

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
