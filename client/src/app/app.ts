be489e558f45765effb9aafeb6b0d77b02528653
Paolo Paci <paolopci@yahoo.it>
2025-08-14 14:26:37 +0200

    add Cors to back-end

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit {
+
+  protected readonly title = signal('DatingApp');
+  http = inject(HttpClient); // invece di usare il costruttore per l'iniezione di dipendenze
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:5001/api/users").subscribe({
+      next: response => {
+        this.users = response,
+          console.log(this.users);
+      },
+      error: error => console.log(error),
+      complete: () => console.log('Request completed')
+    });
+
+  }
+
 }
