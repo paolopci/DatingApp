3e0ef6e087694bc21562bc513a806f606e18ff7b
Paolo Paci <paolopci@yahoo.it>
2025-08-17 15:39:53 +0200

    add ApplicationServiceExtensions and IdentityServiceExtensions for clean refactor program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,5 @@
-using API.Data;
-using API.Interfaces;
-using API.Services;
+using API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 
@@ -10,39 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+// Add extensions for application services and identity services
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-var tokenKey=builder.Configuration["TokenKey"] ??
-                          throw new ArgumentNullException("TokenKey is not found in appsetting.");
-        // controllo anche la lunghezza della chiave
-        if (tokenKey.Length < 64)
-        {
-            throw new Exception("Your tokenKey needs to be longer");
-        }
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(tokenKey)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 
 // Aggiungo l'autenticazione e l'autorizzazione prima di mappare i controller
