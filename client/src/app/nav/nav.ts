c090bccad3b2eff053f8ff002b2380bfcd50103c
Paolo Paci <paolopci@yahoo.it>
2025-08-18 11:51:39 +0200

    create AccountService in client application

--- client/src/app/nav/nav.ts
+++ client/src/app/nav/nav.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account'; // Assuming Account service is in the same directory
 
 @Component({
   selector: 'app-nav',
@@ -9,10 +10,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './nav.css'
 })
 export class Nav {
-
+  private accountService = inject(AccountService)
+  loggedIn = false;
   model: any = {};
 
 
-  login() { }
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    });
+
+  }
 
 }
