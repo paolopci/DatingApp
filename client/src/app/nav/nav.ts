4ce8830a5ee109556868eaa21c080af62d0dcd5a
Paolo Paci <paolopci@yahoo.it>
2025-08-18 13:58:14 +0200

    implement nav.html

--- client/src/app/nav/nav.ts
+++ client/src/app/nav/nav.ts
@@ -1,10 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account'; // Assuming Account service is in the same directory
+import { CommonModule } from '@angular/common';
+// Aggiorna il percorso se necessario
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   standalone: true,
   templateUrl: './nav.html',
   styleUrl: './nav.css'
@@ -21,9 +23,15 @@ export class Nav {
         console.log(response);
         this.loggedIn = true;
       },
-      error: error => console.log(error)
+      error: error => {
+        console.log(error);
+        //  this.loggedIn = false;
+      }
     });
+  }
 
+  logout() {
+    this.loggedIn = false;
   }
 
 }
