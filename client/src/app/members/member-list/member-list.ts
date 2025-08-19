b1444a905ee3f85b86dacab2bed1b2a6292a2174
Paolo Paci <paolopci@yahoo.it>
2025-08-19 14:37:23 +0200

    creo diversi nuovi components

---
+++ client/src/app/members/member-list/member-list.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-member-list',
+  imports: [],
+  standalone: true,
+  templateUrl: './member-list.html',
+  styleUrl: './member-list.css'
+})
+export class MemberList {
+
+}
