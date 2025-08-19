ab1f257d3c18ae82ed737c8faa401b0fb1124932
Paolo Paci <paolopci@yahoo.it>
2025-08-19 15:14:28 +0200

    add routerLink and routerLinkActive for all links in navbar (nav component)

--- client/src/app/nav/nav.ts
+++ client/src/app/nav/nav.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account'; // Assuming Account service is in the same directory
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 // Aggiorna il percorso se necessario
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterLink, RouterLinkActive],
   standalone: true,
   templateUrl: './nav.html',
   styleUrl: './nav.css'
