a96a3a62dcc9e8555317c727f4cb33254f66369d
Paolo Paci <paolopci@yahoo.it>
2025-08-22 14:44:42 +0200

    API: update AppUser and create Photo entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,12 +6,32 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
-        [Required ]
+        [Required]
         public string UserName { get; set; }
+        // [Required]
+        public byte[] PasswordHash { get; set; } = [];
+        //  [Required]
+        public byte[] PasswordSalt { get; set; } = [];
+
+        public DateOnly DateOfBirth { get; set; }
         [Required]
-        public byte[] PasswordHash { get; set; } 
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
         [Required]
-        public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+
+        public string? Introduction { get; set; }
+        public string? Interests { get; set; }
+        public string? LookingFor { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+        public List<Photo> Photos { get; set; } = [];
+
 
     }
 }
---
+++ API/Entities/Photo.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace API.Entities;
+
+public class Photo
+{
+    public int Id { get; set; }
+    [Required] 
+    public string Url { get; set; }
+    public bool IsMain { get; set; }
+    public string? PublicId { get; set; }
+}
