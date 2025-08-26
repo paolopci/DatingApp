1e3a74c795d8e2dcb7e437264bcfa376dfefb49c
Paolo Paci <paolopci@yahoo.it>
2025-08-26 17:32:34 +0200

    fix 1 detail

--- client/src/app/members/member-detail/member-detail.ts
+++ client/src/app/members/member-detail/member-detail.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, input, OnInit, signal, set, effect } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
+import { GalleryModule, Image } from '@ks89/angular-modal-gallery';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-member-detail',
-  imports: [],
+  imports: [CommonModule, GalleryModule],
+  standalone: true,
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
-export class MemberDetail {
+export class MemberDetail implements OnInit {
 
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  //member?: Member
+  member = input<any | null>(null);
+
+  // 2) Aggiungi un signal scrivibile per lo stato locale
+  private memberState = signal<Member | null>(null);
+
+  // 3) Sincronizza automaticamente l'input nel signal locale (se il parent lo fornisce)
+  private syncInput = effect(() => {
+    const fromParent = this.member();
+    if (fromParent) this.memberState.set(fromParent);
+  });
+
+
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: m => {
+        this.memberState.set(m)
+        console.log(m);
+      }
+    });
+  }
+
+  // Computed che trasforma le foto in immagini per la gallery
+  images = computed(() => {
+    const photos = this.member()?.photos ?? [];
+    return photos.map((p: any, index: number) =>
+      new Image(
+        index,
+        { img: p.url, description: p.description },
+        { img: p.url, description: p.description }
+      )
+    );
+  });
+
+  // Facoltativo: getter comodo per il template
+  vm = computed(() => this.memberState());
 }
