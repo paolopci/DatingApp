405db4a68a040ee0a25f1bdb0d730c5a3d7d42b4
Paolo Paci <paolopci@yahoo.it>
2025-08-26 09:38:33 +0200

    Client: Retriving the kist of members

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member } from '../_models/member';
+import { AccountService } from './account';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
+  private accountService = inject(AccountService);
+  private http = inject(HttpClient);
+  baseUrl = environment.apiUrl;
+
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions());
+  }
+
+  getHttpOptions() {
+    return {
+      headers: new HttpHeaders({
+        'Authorization': `Bearer ${this.accountService.currentUser()?.token}`  // è un signal
+      })
+    }
+
+  }
 
 }
--- client/src/app/members/member-list/member-list.ts
+++ client/src/app/members/member-list/member-list.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-list',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './member-list.html',
   styleUrl: './member-list.css'
 })
-export class MemberList {
+export class MemberList implements OnInit {
+  private memberService = inject(MembersService);
+  members: Member[] = [];
+
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: members => this.members = members,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    });
+  }
 
 }
