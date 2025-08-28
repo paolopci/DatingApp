d98da6b9573fe78a0691fb66f1343c9dd2d9734c
Paolo Paci <paolopci@yahoo.it>
2025-08-28 10:41:47 +0200

    add create member-edit component
    update app.routes.ts with routes for member-edit.component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { authGuard } from './_guards/auth-guard';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -17,6 +18,7 @@ export const routes: Routes = [
         children: [
             { path: 'members', component: MemberList },
             { path: 'members/:username', component: MemberDetail },
+            { path: 'member/edit', component: MemberEditComponent },
             { path: 'lists', component: Lists },
             { path: 'messages', component: Messages },
         ]
---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Member } from '../../_models/member';
+import { AccountService } from '../../_services/account';
+import { MembersService } from '../../_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  imports: [],
+  standalone: true,
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css'
+})
+export class MemberEditComponent implements OnInit {
+
+  member?: Member;
+  private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    const user = this.accountService.currentUser();
+    if (!user) return;
+    this.memberService.getMember(user.username).subscribe({
+      next: member => this.member = member,
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    });
+  }
+
+
+}
