e08aeca720f04857401f00f54c04b679c6a9058b
Paolo Paci <paolopci@yahoo.it>
2025-08-29 17:02:15 +0200

    implement cache logic in members.service
    I have implemented the caching logic in members.service.ts as planned. The service will now store and retrieve
      member data from a local signal, falling back to an HTTP request only when necessary. This should provide a
      noticeable performance improvement.

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
+import { map, of, tap } from 'rxjs';
 
 
 @Injectable({
@@ -10,19 +11,30 @@ import { Member } from '../_models/member';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
+  members = signal<Member[]>([]);
 
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members().length > 0) return of(this.members());
+
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      tap(members => this.members.set(members))
+    );
   }
 
   getMember(username: string) {
+    const member = this.members().find(x => x.username === username);
+    if (member) return of(member);
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    console.log(member);
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        this.members.update(members => members.map(m => m.username === member.username ? member : m))
+      })
+    )
   }
 }
 
