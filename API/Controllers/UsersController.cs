d6570ccc522db38f1c181a083c94d8b3e9f06024
Paolo Paci <paolopci@yahoo.it>
2025-09-01 15:30:39 +0200

    API: update UserController
    add and delete  from Cloudinary, impostazioni salvate nel file secret.json

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,10 +1,11 @@
-﻿using System.Security.Claims;
-using API.DTOs;
+﻿using API.DTOs;
 using API.Entities;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using API.Extensions;
 
 
 namespace API.Controllers
@@ -15,14 +16,15 @@ namespace API.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
+        private readonly IPhotoService _photoService;
 
 
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
         {
             _userRepository = userRepository;
             _mapper = mapper;
+            _photoService = photoService;
         }
-
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
@@ -35,31 +37,31 @@ namespace API.Controllers
 
 
         [HttpGet("{id:int}")] // api/users/5
-        public async Task<ActionResult<MemberDto>>  GetUserId(int id)
+        public async Task<ActionResult<MemberDto>> GetUserId(int id)
         {
             var user = await _userRepository.GetUserByIdAsync(id);
-            
+
             if (user == null)
             {
                 return NotFound();
             }
-            
+
             var userToReturn = _mapper.Map<MemberDto>(user);
             return Ok(userToReturn);
 
         }
-        
+
         [HttpGet("{username}")] // api/users/5
-        public async Task<ActionResult<MemberDto>>  GetUser(string username)
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             var user = await _userRepository.GetUserByUsernameAsync(username);
-           
+
             if (user == null)
             {
                 return NotFound();
             }
 
-             var userToReturn = _mapper.Map<MemberDto>(user);
+            var userToReturn = _mapper.Map<MemberDto>(user);
             return Ok(userToReturn);
 
         }
@@ -68,20 +70,50 @@ namespace API.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
         {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            if (username == null) return Unauthorized("Invalid token");
+            //var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            //if (username == null) return Unauthorized("Invalid token");
 
-            var user = await _userRepository.GetUserByUsernameAsync(username);
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
 
             if (user == null) return NotFound();
 
             _mapper.Map(memberUpdateDto, user);
 
             if (await _userRepository.SaveAllAsync()) return NoContent();
-            
+
             return BadRequest("Failed to update user");
         }
 
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+            if (user == null) return BadRequest("Cannot update user");
+
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error != null)
+            {
+                return BadRequest(result.Error.Message);
+            }
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            user.Photos.Add(photo);
+
+            if (await _userRepository.SaveAllAsync())
+            {
+                return _mapper.Map<PhotoDto>(photo);
+            }
+
+            return BadRequest("problem adding photo");
+
+        }
 
     }
 }
