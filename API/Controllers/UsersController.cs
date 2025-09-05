22a77f98bbd2fee77067a9729fdbbb5a42ebd7bf
Paolo Paci <paolopci@yahoo.it>
2025-09-05 10:38:44 +0200

    API: Delete Photo server side

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -143,5 +143,34 @@ namespace API.Controllers
             return BadRequest("Problem setting main photo");
         }
 
+        [HttpDelete("delete-photo/{photoId:int}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)
+        {
+            var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            var photo = user.Photos.FirstOrDefault(x => x.Id == photoId);
+            if (photo == null || photo.IsMain)
+            {
+                return BadRequest("This photo cannot be deleted");
+            }
+
+            if (photo.PublicId != null)
+            {
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if (result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+
+            if (await _userRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Problem deleting photo");
+
+        }
     }
 }
