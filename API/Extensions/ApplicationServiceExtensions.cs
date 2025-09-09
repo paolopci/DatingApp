d7bbb9b762bf5b268765851888188dab07f94c98
Paolo Paci <paolopci@yahoo.it>
2025-09-09 17:25:49 +0200

    API : implement Action Filter
    aggiorno il campo LastActive

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -22,6 +22,7 @@ namespace API.Extensions
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<LogUserActivity>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             
             // Setting per Cloudinary
--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -7,8 +7,11 @@ namespace API.Extensions
     {
         public static string GetUsername(this ClaimsPrincipal user)
         {
-            var username = user.FindFirstValue(ClaimTypes.NameIdentifier)
-                                  ?? throw new Exception("Username claim not found in token.");
+            // Prefer the standard Name claim (mapped from unique_name),
+            // fall back to NameIdentifier only if present for backward compatibility.
+            var username = user.FindFirstValue(ClaimTypes.Name)
+                           ?? user.FindFirstValue(ClaimTypes.NameIdentifier)
+                           ?? throw new Exception("Username claim not found in token.");
 
             return username;
         }
