6ef7fda4e85612bc6cd85e5cc7463a9df6be39a0
Paolo Paci <paolopci@yahoo.it>
2025-09-11 15:40:08 +0200

    Client: Adding the lists component

--- client/src/app/_services/likes.service.ts
+++ client/src/app/_services/likes.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Member } from '../_models/member';
 
 @Injectable({
   providedIn: 'root'
@@ -16,13 +17,12 @@ export class LikesService {
   }
 
   getLikes(predicate: string) {
-    return this.http.get(`${this.baseUrl}likes?predicate=${predicate}`);
+    return this.http.get<Member[]>(`${this.baseUrl}likes?predicate=${predicate}`);
   }
+
   getLikesIds() {
     return this.http.get<number[]>(`${this.baseUrl}likes/list`).subscribe({
       next: ids => this.likeIds.set(ids)
     });
   }
-
-
 }
--- client/src/app/lists/lists.ts
+++ client/src/app/lists/lists.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LikesService } from '../_services/likes.service';
+import { Member } from '../_models/member';
+import { MemberCardComponent } from "../members/member-card/member-card.component";
+import { NgbButtonsModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-lists',
-  imports: [],
+  imports: [MemberCardComponent, FormsModule, NgbButtonsModule],
   templateUrl: './lists.html',
   styleUrl: './lists.css'
 })
-export class Lists {
+export class Lists implements OnInit {
+  private likesService = inject(LikesService);
+  members: Member[] = [];
+  predicate = 'liked';
+
+
+  ngOnInit(): void {
+    this.loadLikes();
+  }
+
+  loadLikes() {
+    this.likesService.getLikes(this.predicate).subscribe({
+      next: members => (this.members = members)
+    });
+  }
+
+  getTitle() {
+    switch (this.predicate) {
+      case 'liked': return 'Members you like';
+      case 'likedBy': return 'Members who like you';
+      default: return 'Mutual';
+    }
+  }
 
 }
